762e5abb8089ae84de4c4c514846652d3d2709c5
Fatima  Nur Fatiha <103501781+Fatema998@users.noreply.github.com>
2024-04-01 22:31:35 +0600

    day_3

---
+++ week_3/day_3/C_1_Powering_the_Hero_easy_version.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int t;
+    cin >> t;
+
+    while (t--)
+    {
+        /* code */
+        int n;
+        cin >> n;
+        vector<long long> a(n);
+        for (int i = 0; i < n; i++)
+        {
+            /* code */
+            cin >> a[i];
+        }
+        long long ans = 0;
+        priority_queue<long long> q;
+
+        for (int i = 0; i < n; i++)
+        {
+            /* code */
+            if (a[i] == 0)
+            {
+                if (q.empty())
+                    continue;
+                ans += q.top();
+                q.pop();
+            }
+            else
+            {
+
+                q.push(a[i]);
+            }
+        }
+        cout << ans << endl;
+    }
+
+    return 0;
+}
